5c20be61682e775256c85924957bfbaa9f3cfdfe
Ana Luna <luna.ana92@gmail.com>
2020-08-28 01:38:27 -0700

    initiated prompts to generate a response for the user's questions

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const inquirer = require('inquirer');
 
 const questions = [
     {
+        type:"input",
+        name: "badge",
+        message: "Enter your badge code"
+    },
+
+    {
         type: "input",
         name: "title",
         message: "What is the title of your project?"
@@ -38,23 +44,98 @@ const questions = [
         ]
     },
     {
-        type:"input",
+        type: "input",
         name: "contributing",
         message: "How can others contribute?"
     },
     {
-        type:"input",
+        type: "input",
         name: "tests",
         message: "Run tests"
     },
     {
-        type:"input",
-        name: "question1",
-        message: "What is your Github username?"
+        type: "input",
+        name: "githubUserName",
+        message: "What is your Github URL?"
     },
     {
-        type:"input",
-        name: "question2",
+        type: "input",
+        name: "email",
         message: "What is your email address?"
     },
 ]
+
+inquirer
+    .prompt(questions).then(response =>{
+
+        fs.appendFileSync("./example-README.md", ("\n" + response.badge) + '\n', function(error) {
+            if (error){
+                return console.log(error);
+            }
+            console.log("success!")
+        });
+
+        fs.appendFileSync("./example-README.md", ("#" + response.title) + '\n', function(error) {
+            if (error){
+                return console.log(error);
+            }
+            console.log("success!")
+        });
+
+        fs.appendFileSync("./example-README.md", ("## Description" + '\n' + response.description) + '\n', function(error) {
+            if (error){
+                return console.log(error);
+            }
+            console.log("success!")
+        });
+
+        fs.appendFileSync("./example-README.md", ("## Table of Contents" + '\n' + response.content) + '\n', function(error) {
+            if (error){
+                return console.log(error);
+            }
+            console.log("success!")
+        });
+
+        fs.appendFileSync("./example-README.md", ("## Installation" + '\n' + response.installation) + '\n', function(error) {
+            if (error){
+                return console.log(error);
+            }
+            console.log("success!")
+        });
+
+        fs.appendFileSync("./example-README.md", ("## Usage" + '\n' + response.usage) + '\n', function(error) {
+            if (error){
+                return console.log(error);
+            }
+            console.log("success!")
+        });
+
+        fs.appendFileSync("./example-README.md", ("## License" + '\n' + response.license) + '\n', function(error) {
+            if (error){
+                return console.log(error);
+            }
+            console.log("success!")
+        });
+
+        fs.appendFileSync("./example-README.md", ("## Contributing" + '\n' + response.contributing), function(error) {
+            if (error){
+                return console.log(error);
+            }
+            console.log("success!")
+        });
+
+        fs.appendFileSync("./example-README.md", ("## Tests" + '\n' + response.tests) + '\n', function(error) {
+            if (error){
+                return console.log(error);
+            }
+            console.log("success!")
+        });
+
+        fs.appendFileSync("./example-README.md", ("## Questions" + '\n' + response.githubUserName + '\n' + response.email), function(error) {
+            if (error){
+                return console.log(error);
+            }
+            console.log("success!")
+        });
+
+    });
